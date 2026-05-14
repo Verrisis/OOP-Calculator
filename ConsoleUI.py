b97613719ab1cab61642e937df0319790e368346
Verrisis <dvorosilov0808@gmail.com>
2026-05-14 14:05:19 +0200

    refactor: add more tests and apply SRP fixes

--- ConsoleUI.py
+++ ConsoleUI.py
@@ -1,4 +1,5 @@
 from Calculator import Calculator
+from EmptyHistoryError import EmptyHistoryError
 from FileManager import FileManager
 
 
@@ -27,16 +28,22 @@ class ConsoleUI:
             try:
                 linia = input("> ").strip()
                 if linia.lower().startswith("pomoc"):
-                    print(f"Dostępne komandy:\n1) Debug\n 2)Zapisz *imię pliku*\n 3)Wczytaj *imię pliku*\n 4)Szereg *drob*")
+                    print(
+                        f"Dostępne komendy:\n1) Debug\n2) Zapisz *imię pliku*\n3) Wczytaj *imię pliku*\n4) Szereg *ułamek*")
                     continue
-                if linia.lower() in ['exit', 'quit']:
-                    self.print_history()
-                    break
                 if linia.lower().startswith("zapisz"):
                     parts = linia.split()
-                    FileManager.save_to_file(self.calc, parts[1])
-                    print(f"Zapisano historię do pliku {parts[1]}")
-                    continue #Like Ctrl + S in Word never closes your document
+                    if len(parts) == 2:
+                        try:
+                            FileManager.save_to_file(self.calc, parts[1])
+                            print(f"Zapisano historię do pliku {parts[1]}")
+                        except EmptyHistoryError:
+                            print("Historia jest pusta. Nie zapisuje pliku")
+                        except Exception as e:
+                            print(f"Błąd zapisu do pliku: {e}")
+                    else:
+                        print("Błąd: Podaj nazwę pliku, np. 'zapisz wyjscie.txt'")
+                    continue
                 if linia.lower().startswith("wczytaj"):
                     parts = linia.split()
                     if len(parts) == 2:
@@ -49,25 +56,29 @@ class ConsoleUI:
                     print(f"Info do debugowania:")
                     self.calc.history[-1].debug_info()
                     continue
+                if linia.lower() in ['exit', 'quit']:
+                    self.print_history()
+                    break
                 if not linia:
                     continue
 
-                wynik = self.calc.parse_and_compute(linia)
-                self.calc.history.append(wynik)
+                wynik = self.calc.evaluate(linia)
+                self.calc.add_to_history(wynik)
                 print(f"Wynik: {wynik}")
 
             except EOFError:
-                self.calc.err_history.append("EOFError")
+                self.calc.add_to_err_history("EOFError")
                 break
             except (ValueError, ZeroDivisionError, IndexError, TypeError) as e:
-                self.calc.err_history.append(f"Błąd: {e}")
+                self.calc.add_to_err_history(f"Błąd: {e}")
                 print(f"Błąd: {e}")
             except KeyboardInterrupt:
                 print("Działanie przerwano za pomocą klawiatury")
                 break
             except Exception as e:
-                self.calc.err_history.append(f"Niespodziewany Błąd: {e}")
+                self.calc.add_to_err_history(f"Niespodziewany Błąd: {e}")
                 print(f"Niespodziewany Błąd: {e}")
+
 
 if __name__ == "__main__":
     with ConsoleUI() as ui:
--- FileManager.py
+++ FileManager.py
@@ -1,21 +1,35 @@
+from EmptyHistoryError import EmptyHistoryError
+
+
 class FileManager:
     @staticmethod
     def load_from_file(calc, filename):
-        with open(filename, "r") as f:
-            for line in f:
-                clean_line = line.strip()
-                if not clean_line: continue
-                try:
-                    fraction = calc.parse_and_compute(clean_line)
-                    calc.history.append(fraction)
-                except (ValueError, ZeroDivisionError, IndexError, TypeError) as e:
-                    calc.err_history.append(f'BŁĄD: {e}')
+        try:
+            with open(filename, "r") as f:
+                for line in f:
+                    clean_line = line.strip()
+                    if not clean_line: continue
+
+                    try:
+                        fraction = calc.evaluate(clean_line)
+                        calc.add_to_history(fraction)
+                    except (ValueError, ZeroDivisionError, IndexError, TypeError) as e:
+                        calc.add_to_err_history(f'BŁĄD: {e}')
+
+        except Exception as file_e:
+            print(f"Błąd systemu plików podczas wczytywania: {file_e}")
 
     @staticmethod
     def save_to_file(calc, filename):
-        with open(filename, "w") as f:
-            for fraction in calc.history:
-                f.write(f"{fraction}\n")
-        with open("err_" + filename, "w") as f1:
-            for error in calc.err_history:
-                f1.write(f"{error}\n")
+        if not calc.history and not calc.err_history:
+            raise EmptyHistoryError
+
+        if calc.history:
+            with open(filename, "w") as f:
+                for fraction in calc.history:
+                    f.write(f"{fraction}\n")
+
+        if calc.err_history:
+            with open("err_" + filename, "w") as f1:
+                for error in calc.err_history:
+                    f1.write(f"{error}\n")
